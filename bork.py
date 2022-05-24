e8f6a57ae572d95780b233e2877c2b4ae2c21a7c
corentintran <102200377+corentintran@users.noreply.github.com>
2022-05-24 15:43:42 +1000

    start

--- bork.py
+++ bork.py
@@ -2,9 +2,39 @@ import graphs
 import digraphs
 
 # You can define some helper functions here if you like!
+def breadthFirstBork(V, E, map):
+     V0 = V              # V_0 = V
+   D = [ {u} ]         # D[0] = D_0 = {u}
+   return distanceClassesR(V0, E, D)
+
+
+
+def breadthFirstBorkR(V, E, map):
+   Vnew = V - D[-1]            # V_{j} = V_{j-1} / D_{j-1}
+   if len(Vnew) == 0: return D # Already considered all elements?
+   Dnew = D + [ NS_out(Vnew, E, D[-1]) ]  # D_{j} = N_{V_j}(D_{j-1})
+   return distanceClassesR(Vnew, E, Dnew)
+
 
 def traverseBork(bork):
    # code your solution here
+
+   map = {}
+   bork.__init__()
+
+   E = bork.exits()
+
+   breadthFirstBorkR(bork, map)
+
+   saveNo = bork.save()
+   exits = bork.exits()
+   for e in exits :
+      bork.move(e)
+      loc = bork.description
+      if loc in set(map.keys()):
+         bork.restore(saveNo)
+   //visit exit
+   //if 
 
    # Access the bork automator like so:
    bork.restart()
